c5d892b7e6fb61f85a62424761faa0af2d57e88a
Yookyung Kim <37654724+tituvely@users.noreply.github.com>
2021-05-24 13:45:33 +0900

    DP

--- Baekjoon/BOJ2169.java
+++ Baekjoon/BOJ2169.java
@@ -1,15 +1,13 @@
+package dp;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.Buffer;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
-
-import jdk.internal.org.jline.utils.InputStreamReader;
-
 //2169. 로봇 조종하기
-
-
 public class BOJ2169 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer stz;
@@ -17,34 +15,112 @@ public class BOJ2169 {
     static int weight[][];
     static int dp[][];
 
-    public static void main(String[] args) {
+    static int INF = 1000000;
+    static int cache[][][];
+    static boolean visit[][];
+    static int dx[] = {0, 0, 1};
+    static int dy[] = {-1, 1, 0};
+
+    public static void main(String[] args) throws IOException {
         stz = new StringTokenizer(br.readLine());
         N = Integer.parseInt(stz.nextToken());
         M = Integer.parseInt(stz.nextToken());
         weight = new int[N+1][M+1];
+        //DP로 푸는 경우
         dp = new int[N+1][M+1];
+
+        //DFS로 푸는 경우
+        cache = new int[N+1][M+1][3];
+        visit = new boolean[N+1][M+1];
 
         for(int i = 1; i <= N; i++) {
             stz = new StringTokenizer(br.readLine());
             for(int j = 1; j <= M; j++) {
-                weight[i][j] = Integer.parseInt(stz.nextToken());;
+                weight[i][j] = Integer.parseInt(stz.nextToken());
+                cache[i][j][0] = cache[i][j][1] = cache[i][j][2] = -INF;
             }
         }
 
-        int [][] visited = new int[N+1][M+1];
+        //DP로 푸는 경우
+        // findMaxValue();
 
-        System.out.println(findMaxValue(0, 0, visited));
+        //DFS로 푸는 경우
+        visit[1][1] = true;
+        
+        System.out.println( dfs(1, 1, 0) );
     }
 
-    public static int findMaxValue(int n, int m, int[][] visited) {
+    public static void findMaxValue() {
+
         // 로봇은 왼쪽, 오른쪽, 아래쪽으로만 이동할 수 있다.
-        // 이미 방문한 지점일 경우 값을 갱신하지 않는다.
 
         // 시작점
-        if(n == 0 && m == 0) dp[0][0] = weight[0][0];
+        dp[1][1] = weight[1][1];
 
-        // 왼쪽으로 이동
-        if(dp[])
-        
+        // 첫째줄
+        // 첫 줄은 왼쪽에서 오는 경우 밖에 없다
+        for(int j = 2; j <= M; j++) {
+            dp[1][j] = dp[1][j-1] + weight[1][j];
+        }
+
+        // 둘째줄
+        // dp[n][m] = 왼쪽에서 올 때, 오른쪽에서 올 때, 위에서 올 때의 최댓값 + weight[n][m]
+        // for문을 돌려서 내려올 때 위에서 내려온 경우는 최댓값이 갱신된 상태이지만
+        // 왼쪽에서 오거나 오른쪽에서 온 경우는 최댓값이 갱신된 상태가 아니다
+        // 따라서, 각 row에 대해 왼쪽에서 오는 경우와 오른쪽에서 오는 경우를 따로 계산해야 한다
+        // 이를 위해 배열 두개를 만든다
+        int left[], right[];
+        for(int i = 2; i <= N; i++) {
+            //왼쪽에서 오는 경우
+            //왼쪽에서 올 때 VS 위에서 올 때 중에 최댓값을 구해 저장한다
+            left = new int[M+1];
+            left[0] = dp[i-1][1];
+            for(int j = 1 ; j <= M; j++) {
+                left[j] = Math.max(left[j-1], dp[i-1][j]) + weight[i][j];
+            }
+            
+            //오른쪽에서 오는 경우
+            //오른쪽에서 올 때 VS 위에서 올 때 중에 최댓값을 구해 저장한다
+            right = new int[M+2];
+            right[M+1] = dp[i-1][M];
+            for(int j = M; j >=1 ; j--) {
+                right[j] = Math.max(right[j+1], dp[i-1][j]) + weight[i][j]; 
+            }
+
+            //left[i]와 right[i] 중에 더 큰 값은 왼쪽, 오른쪽, 위쪽에서 오는 모든 경우에 대한 최댓값이 된다
+            for(int j = 1; j <= M; j++) {
+                dp[i][j] = Math.max(left[j], right[j]);
+            }
+        }
+       
+        System.out.println(dp[N][M]);
     }
+
+    //dir 방향에서 온 y,x
+    public static int dfs(int x, int y, int dir) {
+        // 로봇은 왼쪽, 오른쪽, 아래쪽으로만 이동할 수 있다.
+
+        if (x == N && y == M) return weight[x][y];
+    
+        //이미 값이 계산되어 있는 경우 반환
+        if (cache[x][y][dir] != -INF) return cache[x][y][dir];
+        
+        // 아닌 경우 계산
+        for (int i = 0; i < 3; i++) {
+            //0 왼쪽 1 오른쪽 2 아래쪽
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            System.out.println(x + " " + y + " ");
+            System.out.println(nx + " " + ny + " ");
+
+            if (nx < 1 || nx > N || ny < 1 || ny > M || visit[nx][ny]) continue;
+
+            visit[nx][ny] = true;
+            cache[x][y][dir] = Math.max(cache[x][y][dir], dfs(nx, ny, i) + weight[x][y]);
+            visit[nx][ny] = false;
+        }
+        return cache[x][y][dir];
+    }
+
 }
--- Baekjoon/DP_Palindrome_divide.java
+++ Baekjoon/DP_Palindrome_divide.java
@@ -1,56 +1,57 @@
-//1509. 팰린드롬 분할
+// 1509.팰린드롬 분할
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class DP_Palindrome_divide {
-    static int dp[][];
-    // 시작점 1 -> N까지의 숫자까지의 팰린드롬 분할 최소 갯수
-    static int dp_div[];
-    static int n;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static boolean[][] dp;
     static String str;
+    static int N;
+    static int[] dp_div;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws NumberFormatException, IOException {
         str = br.readLine();
-        n = str.length();
-        dp = new int[n+1][n+1];
-        dp_div = new int[n+1];
+        N = str.length();
+        dp = new boolean[N+1][N+1];
+        dp_div = new int[N+1];
+        Arrays.fill(dp_div, 2*N);
 
         palindrome();
         palindrome_divide();
 
-        System.out.println(dp_div[n]);
+        System.out.println(dp_div[N]);
     }
 
     public static void palindrome() {
         // 길이가 1일때 계산
-        for(int i = 1; i <= n; i++)
-            dp[i][i] = 1;
+        for(int i = 1; i <= N; i++)
+            dp[i][i] = true;
 
         // 길이가 2일때 계산
-        for(int i = 1; i <= n - 1; i++)
-            if(str.charAt(i-1) == str.charAt(i+1-1)) dp[i][i + 1]= 1;
+        for(int i = 1; i <= N - 1; i++)
+            if(str.charAt(i - 1) == str.charAt(i)) dp[i][i + 1]= true;
 
         // 길이가 3 이상일 때
-        for(int i = 2; i < n; i++){
-            for(int j = 1; j <= n - i; j++){
-                if(str.charAt(j-1) == str.charAt(j+1-1) && dp[j + 1][j + i - 1]==1)
-                    dp[j][j + i] = 1;
+        for(int i = 2; i < N; i++){
+            for(int j = 1; j <= N - i; j++){
+                if(str.charAt(j - 1) == str.charAt(j + i -1) && dp[j + 1][j + i - 1])
+                    dp[j][j + i] = true;
             }
         }
     }
 
     public static void palindrome_divide() {
+        // dp_div[n]는 1부터 N까지의 분할의 개수의 최솟값
         dp_div[0] = 0;
-        dp_div[1] = 1;
-        // dp_div[2]는 1 또는 2
-        // dp_div[2] = Math.min(dp_div[0] + dp[1][2], dp_div[1] + 1);
-
-        for(int i = 0; i < n-1 ; i++) {
-            if(dp[i+1][n]==1) {
-                dp_div[n] = Math.min(dp_div[i] + dp[i+1][n], dp_div[i-1] + 1);
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= i; j++) {
+                if(dp[j][i]) {
+                    dp_div[i] = Math.min(dp_div[j-1] + 1, dp_div[i]);
+                }
             }
-        } 
+        }         
     }
-}
 
+}
