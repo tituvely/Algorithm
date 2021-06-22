e8d8939f7e29b7b149362a365bb96954e06b15cc
Yookyung Kim <37654724+tituvely@users.noreply.github.com>
2021-06-22 17:33:28 +0900

    교육P-0023. 의미있는 문장 찾기

--- Samsung/SW_P0023.java
+++ Samsung/SW_P0023.java
@@ -1,12 +1,13 @@
 // 교육P-0023. 의미있는 문장 찾기
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class SW_P0023 {
     static int T, N, K;
     static int[] word;
-    static int[][] dp;
+    static int[] cnt;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,8 +20,10 @@ public class SW_P0023 {
             K = Integer.parseInt(stz.nextToken());
             stz = new StringTokenizer(br.readLine());
             word = new int[N];
+            cnt = new int[N + 1];
             for(int i = 0 ; i < N; i++) {
                 word[i] = Integer.parseInt(stz.nextToken());
+                cnt[word[i]]++;
             }
 
             System.out.println("#" + t + " " + meaningful());
@@ -30,39 +33,68 @@ public class SW_P0023 {
     public static int meaningful() {
         int minCnt = -1;
 
-        dp = new int[N][N];
-        for(int i = 0 ; i < N; i++) {
-            dp[i][i] = 1;
-        }
-
-        for(int i = 0 ; i < N - 1; i++) {
-            if(word[i] != word[i + 1]) {
-                dp[i][i + 1] = 2;
-            } else {
-                dp[i][i + 1] = 1;
+        // 전체 단어 종류의 개수가 K보다 작을 경우
+        for(int i = 1 ; i <= N; i++) {
+            if(cnt[i] != 0) {
+                cnt[0]++;       
             }
-        }
-
-        for(int i = 2; i < N; i++) {
-            for(int j = 0; j + i < N; j++) {
-                if (countWord(j, j + i) == K) {
-                    minCnt = i;
+            if(i == N) {
+                if (cnt[0] < K) {
                     return minCnt;
                 }
             }
         }
 
-        return minCnt;
-    }
+        Arrays.fill(cnt, 0);
+        int l = 0, r = K - 1;
+        for(int i = l ; i <= r; i++) {
+            if(cnt[word[i]] == 0) {
+                cnt[0]++;
+            } 
+            cnt[word[i]]++;
+        }
 
-    public static int countWord(int start, int end) {
-        for(int i = start; i < end; i++) {
-            if(word[i] == word[end]) {
-                dp[start][end] = dp[start][end - 1];
-                return dp[start][end];
+        if(cnt[0] == K) {
+            minCnt = K;
+            return minCnt;
+        } else {
+            r++;
+        }
+        
+        while(true) {
+            // 단어의 개수가 K보다 작을 경우
+            if(r - (l - 1) < K) {
+                r++;
+            }
+
+            // 끝까지 탐색했을 경우
+            if(r > N) {
+                return minCnt;
+            }
+
+            // 앞 단어와 마지막 단어가 같을 경우 앞의 인덱스를 증가시켜 줌
+            if(word[l] == word[r]) {
+                l++;
+                r++;
+                continue;
+            }
+            // 다를 경우, 마지막 단어가 새로운 단어인지 체크해 줌
+            else {
+                if(cnt[word[r]] == 0) {
+                    cnt[0]++;
+                }
+                cnt[word[r]]++;
+            }
+            
+            if(cnt[0] == K) {
+                minCnt = r - (l - 1);
+                return minCnt;
+            } else {
+                r++;
+                if(cnt[word[l]] > 1) {
+                    l++;
+                }
             }
         }
-        dp[start][end] = dp[start][end - 1] + 1;
-        return dp[start][end - 1];
     }
 }
