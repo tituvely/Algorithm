e3a785ba8c72d2511262107960bd3960a106f96f
Yookyung Kim <37654724+tituvely@users.noreply.github.com>
2021-06-23 11:40:14 +0900

    교육P-0020. 달리기

--- Samsung/SW_P0020.java
+++ Samsung/SW_P0020.java
@@ -5,7 +5,8 @@ import java.util.StringTokenizer;
 
 public class SW_P0020 {
     static int T, N;
-    static int[] runCapa;
+    static long[] runCapa;
+    static long cnt;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,24 +15,60 @@ public class SW_P0020 {
         T = Integer.parseInt(br.readLine());
         for(int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
+            runCapa = new long[N];
+
             stz = new StringTokenizer(br.readLine());
-            runCapa = new int[N];
             for(int i = 0; i < N; i++) {
                 runCapa[i] = Integer.parseInt(stz.nextToken());
             }
-            System.out.println("#"+ t + " " + reverse());
+
+            cnt = 0;
+            mergeSort(runCapa, 0, runCapa.length - 1);
+
+            System.out.println("#"+ t + " " + cnt);
         }
     }    
 
-    public static int reverse() {
-        int cnt = 0;
-        for(int i = 1; i < N; i++) {
-            for(int j = 0; j < i; j++) {
-                if(runCapa[j] < runCapa[i])
-                    cnt++;
-            }  
+    public static void mergeSort(long[] array, int start, int end) {
+        if(start >= end) return;
+
+        int mid = (start + end) / 2;
+
+        mergeSort(array, start, mid);
+        mergeSort(array, mid+1, end);
+        merge(array, start, mid, mid+1, end);
+    }
+
+    public static void merge (long[] array, int lstart, int lend, int rstart, int rend) {
+        int lmin, rmin, indexTemp;
+        long[] temp = new long[(rend-lstart)+1];
+
+        lmin = lstart;
+        rmin = rstart;
+        indexTemp = 0;
+
+        while (lmin <= lend && rmin <= rend) {
+            if (array[lmin] < array[rmin]) {
+                temp[indexTemp++] = array[rmin++];
+                cnt = cnt + (lend - lmin + 1); 
+            } else {
+                temp[indexTemp++] = array[lmin++];
+            }
         }
-        return cnt;
+
+        if(lmin > lend) {
+            for(int i=rmin; i<=rend; i++) {
+                temp[indexTemp++] = array[i];
+            }
+        } else {
+            for(int i=lmin; i<=lend; i++) {
+                temp[indexTemp++] = array[i];
+            }
+        }
+
+        for(int i = lstart; i<=rend; i++) {
+            array[i] = temp[i-lstart];
+        }
     }
 
 }
