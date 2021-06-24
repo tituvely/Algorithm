505da905824517ced957b8973b608797cbd06d23
Yookyung Kim <37654724+tituvely@users.noreply.github.com>
2021-06-24 15:07:14 +0900

    연습A-0022. 소수경로

--- Samsung/SW_A0022.java
+++ Samsung/SW_A0022.java
@@ -31,8 +31,11 @@ public class SW_A0022 {
             A = Integer.parseInt(stz.nextToken());
             B = Integer.parseInt(stz.nextToken());
 
+            visited = new int[MAX];
+            int result = bfs(A, B) - 1; //visited[start] = 1로 시작했으므로 -1 해줘야 함
 
-
+            bw.write("#" + t + " " + result);
+            bw.newLine();
         }
         
         bw.flush();
@@ -54,6 +57,11 @@ public class SW_A0022 {
 
             // 현재소수에서 숫자 하나만 바꿔서 갈 수 있는 소수를 모두 queue에 담는다
             for(int i = 0; i < 4; i++) {
+                // (current % (offset[i] * 10) / offset[i]) -> 자릿수를 구함
+                // ex) (1234 % 10) / 1 = 4
+                //     (1234 % 100) / 10 = 3
+                //     (1234 % 1000) / 100 = 2
+                // (current % (offset[i] * 10) / offset[i]) * offset[i] -> 자릿수를 구하고 offset을 곱해서 빼줘서 해당 자릿수를 0으로 만듦
                 int changeDigit = current - (current % (offset[i] * 10) / offset[i]) * offset[i];
                 for(int j = 0; j < 10; j++) {
                     int next = changeDigit + (offset[i] * j);
@@ -67,7 +75,6 @@ public class SW_A0022 {
             }
         }
 
-
         return 0;
     }
 
@@ -77,13 +84,15 @@ public class SW_A0022 {
             if(primeNum[i]) {   //소수이면
                 //for loop 배수들을 모두 제외
                 for(int j = i * i; j <= MAX; j += i) {  // i * i보다 작은 값들은 이미 이전 i의 배수를 체크할 때 다 체크했음
-                    primeNum[j] = false;    //소수의 배수는 모두 소수가 아니다.
+                    primeNum[j] = false;    // 소수의 배수는 모두 소수가 아니다.
                 }
             }
         }
 
-
-
+        // 문제의 조건에 맞게 1000보다 작은 구간은 전부 false로 해서 못가게 함
+        for(int i = 0; i <= 1000; i++) {
+            primeNum[i] = false;
+        }
     }
 
 }
